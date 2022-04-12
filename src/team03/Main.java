8a4877a6c56e5bc34fc6e072c02f5ce02ff44219
email <white487@naver.com>
2022-04-12 13:11:03 +0900

    메인파일-지훈

--- src/team03/Main.java
+++ src/team03/Main.java
@@ -1,7 +1,40 @@
 package team03;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		Name n = null;
+		ArrayList<Name> arr = new ArrayList<Name>();
+		String name;
+		System.out.println("회원관리 프로그램");
+		
+		while(true) {
+			int num;
+			num = sc.nextInt();
+			switch(num) {
+				case 1:
+					System.out.println("회원등록 합니다.- 지훈");
+					
+					break;
+				case 2:
+					System.out.println("점심시간 확인 - 최지언");
+					break;
+				case 3:
+					System.out.println("등록된 회원 검색 - 양동훈");
+					break;
+				case 4:
+					System.out.println("구구단 ( 2단부터 19단까지) - 예");
+			
+			}
+		}
+		
+		
+		
+		
+		
 		
 	}
 }
--- src/team03/management.java
+++ src/team03/management.java
@@ -3,3 +3,14 @@ package team03;
 public interface management {
 	public void management();
 }
+
+class Name {
+	private String name;
+	
+	public void setName() {
+		this.name = name;
+	}
+	public String getName() {
+		return name;
+	}
+}
