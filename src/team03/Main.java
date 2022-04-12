14fe99e77535dbc2af162a75998815b43a100ede
Punghwi <white487@naver.com>
2022-04-12 21:06:03 +0900

    지훈완료

--- src/team03/Main.java
+++ src/team03/Main.java
@@ -3,22 +3,23 @@ package team03;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import jihunLogin.Login;
 import jiwonMenu.LunchFunc;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		Name n = null;
-		ArrayList<Name> arr = new ArrayList<Name>();
-		String name;
-		System.out.println("회원관리 프로그램");
+		boolean bool = true;
+		System.out.println("1조 기능 프로그램");
 		
-		while(true) {
+		while(bool) {
+			System.out.println("1.지훈 2.지원 3.동훈 4.예린 5.종료");
 			int num;
 			num = sc.nextInt();
 			switch(num) {
 				case 1:
-					System.out.println("회원등록 합니다.- 지훈");
+					Login lo = new Login();
+					lo.display();
 					break;
 				case 2:
 					LunchFunc lm = new LunchFunc();
@@ -29,7 +30,9 @@ public class Main {
 					break;
 				case 4:
 					System.out.println("구구단 ( 2단부터 19단까지) - 예");
-			
+					break;
+				case 5:
+					bool = false;
 			}
 		}
 		
